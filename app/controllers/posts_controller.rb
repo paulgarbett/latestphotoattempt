7247c17ddb49d9fc59bae1db88f1a66dfe3d2240
paulgarbett <paulcgarbett@gmail.com>
2014-11-08 23:21:26 -0800

    fixed login/logout display

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,33 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+  def create
+		@post = Post.new(params.require(:post).permit(:title, :camera, :desc))
+		if @post.save
+				redirect_to posts_path
+		else
+				render 'new'
+		end
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+  	if @post.update_attributes(params.require(:post).permit(:title, :camera, :desc))
+  		redirect_to post_path
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+  	redirect_to posts_path
+  end
+
+
 end
